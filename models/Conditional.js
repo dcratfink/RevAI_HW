465706054581f5de4763b204a3f1289eeaba296b
Ricky Ratliff <98427316+rratlif2@users.noreply.github.com>
2025-09-17 13:54:35 -0400

    Change copy and paste errors and corrected Conditional Model

--- models/Conditional.js
+++ models/Conditional.js
@@ -1,14 +1,14 @@
+import mongoose from "mongoose";
 
-function createConditionalNode(id, field, operator, value, onTrueNextNodeId, onFalseNextNodeId) {
-    return {
-        id: id,
-        type: 'CONDITIONAL',
+const ConditionalSchema = new mongoose.Schema({
+    id: { type: Number, required: true, unique: true },
+    node_type: 'CONDITIONAL',
         condition: {
             field: field,
             operator: operator,
-            value: value
-        },
-        on_true_next_node_id: onTrueNextNodeId,
-        on_false_next_node_id: onFalseNextNodeId
-    };
-}
+            value: value        },
+        on_true_next_node_id: { type: Number, required: false, default: 0 },
+        on_false_next_node_id: { type: Number, required: false, default: 0 },
+});
+
+module.exports = mongoose.model('Conditional', ConditionalSchema);
