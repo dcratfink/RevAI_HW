41527e4b627b2d62252dd40fc753f07ba9e4e61b
Ricky Ratliff <98427316+rratlif2@users.noreply.github.com>
2025-09-17 13:08:26 -0400

    Patient model initial commit

---
+++ models/Patient.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const PatientSchema = new mongoose.Schema({
+    id: { type: Number, required: true, unique: true },
+    age:   { type: Number, required: false, default: null },
+    language: { type: String, required: false, default: "en" },
+    next_node_id: { type: Number, required: false, default: null },
+    condition: { type: String, required: false, default: "unknown" },
+});
+
+module.exports = mongoose.model('Action', PatientSchema);
