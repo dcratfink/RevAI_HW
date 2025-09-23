320b7926edf1475bbc8afc24729cf9fc0c46b291
Ricky Ratliff <98427316+rratlif2@users.noreply.github.com>
2025-09-23 10:49:12 -0400

    Put updates in for latest commit

--- models/Action.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ActionSchema = new mongoose.Schema({
-    message:  { type: String, required: false, default: "" },
-    nextNodeId: { type: Number, required: false, default: null },
-    nodeType: "MESSAGE",
-});
-
-module.exports = mongoose.model('action', ActionSchema);
--- models/Delay.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DelaySchema = new mongoose.Schema({
-    duration:  { type: Number, required: true, default: 0.00 },
-    nextNodeId: { type: Number, required: false, default: null },
-    nodeType: "DELAY",
-});
-
-module.exports = mongoose.model('delay', DelaySchema);
